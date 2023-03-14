4700ccf2f1698c1ddbe0fc601500d0cc6e100852
Giorgio Giannotta <jorge_upa@msn.com>
2023-03-14 18:26:21 +0100

    Add Botton Bar
    Add Botton Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,17 +30,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _currentIndex = 0;
+
+  List<Widget> pageList = [
+    Center(
+      child: Text("Races List"),
+    ),
+    Center(
+      child: Text("Circuits Map"),
+    ),
+    Center(
+      child: Text("Constructors"),
+    ),
+    Center(
+      child: Text("Developer Info"),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: pageList[_currentIndex],
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          /// Home
+          SalomonBottomBarItem(
+            icon: Icon(Icons.flag_outlined),
+            title: Text("Races"),
+            selectedColor: Colors.purple,
+          ),
+
+          /// Likes
+          SalomonBottomBarItem(
+            icon: Icon(Icons.map_outlined),
+            title: Text("Circuits Map"),
+            selectedColor: Colors.pink,
+          ),
+
+          /// Search
+          SalomonBottomBarItem(
+            icon: Icon(Icons.drive_eta),
+            title: Text("Constructors"),
+            selectedColor: Colors.orange,
+          ),
+
+          /// Profile
+          SalomonBottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Developer Info"),
+            selectedColor: Colors.teal,
+          ),
+        ],
       ),
     );
   }
