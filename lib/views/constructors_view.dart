6a53fad6c5fd8d56b5aad926a2ab5b000fbd1635
Giorgio Giannotta <jorge_upa@msn.com>
2023-06-01 21:19:57 +0200

    Refactor with FutureBuilder

--- lib/views/constructors_view.dart
+++ lib/views/constructors_view.dart
@@ -14,58 +14,65 @@ class _ConstructorsViewState extends State<ConstructorsView> {
   @override
   void initState() {
     super.initState();
-      fetchWords();
-      print("cachedWords Empty");
-
   }
 
-  fetchWords() async {
-    constructors = (await constructorsViewModel.fetchConstructors())!;
-    setState(() {});
-  }
+  // fetchConstructors() async {
+  //   constructors = (await constructorsViewModel.fetchConstructors())!;
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Constructors'),
-      // ),
-      body: ListView.builder(
-        itemCount: constructorsViewModel.constructors.length,
-        itemBuilder: (context, index) {
-          final constructor = constructorsViewModel.constructors[index];
-          return ListTile(
-            title: Text(
-              constructor.name,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Nationality: ${constructor.nationality} ${constructorsViewModel.constructorNationalityFlag(constructor.nationality)}'),
-                Text('Power Unit: ${constructorsViewModel.constructorPowerUnit(constructor.name)}'),
-                Text('Drivers: ${constructorsViewModel.constructorDrivers(constructor.name)[0]} - ${constructorsViewModel.constructorDrivers(constructor.name)[1]}'),
-              ],
-            ),
-            trailing: SizedBox(
-              width: 60,
-              height: 60,
-              child: Image.network(
-                constructorsViewModel.constructorImageUrl(constructor.name),
-                errorBuilder: (context, error, stackTrace) => Icon(Icons.image_not_supported),
-              ),
-            ),
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => ConstructorDetailsView(constructor: constructor),
-              //   ),
-              // );
-            },
-          );
+      body: FutureBuilder<List<Constructors>?>(
+        future: constructorsViewModel.fetchConstructors(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            constructors = snapshot.data!;
+            return ListView.builder(
+              itemCount: constructors.length,
+              itemBuilder: (context, index) {
+                final constructor = constructors[index];
+                return ListTile(
+                  title: Text(
+                    constructor.name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Nationality: ${constructor.nationality} ${constructorsViewModel.constructorNationalityFlag(constructor.nationality)}'),
+                      Text('Power Unit: ${constructorsViewModel.constructorPowerUnit(constructor.name)}'),
+                      Text('Drivers: ${constructorsViewModel.constructorDrivers(constructor.name)[0]} - ${constructorsViewModel.constructorDrivers(constructor.name)[1]}'),
+                    ],
+                  ),
+                  trailing: SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: Image.network(
+                      constructorsViewModel.constructorImageUrl(constructor.name),
+                      errorBuilder: (context, error, stackTrace) => Icon(Icons.image_not_supported),
+                    ),
+                  ),
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => ConstructorDetailsView(constructor: constructor),
+                    //   ),
+                    // );
+                  },
+                );
+              },
+            );
+          }
         },
       ),
     );
   }
+
 }
